a4c085635a5c2bb97227648dead71e266a85f42e
Eder Henrique <ederhscc@gmail.com>
2023-08-15 10:01:42 -0300

    First commit

---
+++ src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Parabrisa", "Farol", "Buzina"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Pêssego", "Tangerina", "Melão"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Ouvidos", "Nariz", "Joelho"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Câmera", "Fonte", "Microfone", "Teclas"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Java", "Html", "CSS", "Express"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Peixe", "Frango", "Macarrão"]
+};
